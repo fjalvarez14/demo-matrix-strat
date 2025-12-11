7b82f51fb8f2c4b5463440282b51ac928e06b279
Francisco Alvarez <fjalvarezr14@gmail.com>
2025-12-11 15:12:04 +0100

    Change external ping test to use GitHub API

--- tests/e2e/test_e2e_example.py
+++ tests/e2e/test_e2e_example.py
@@ -1,5 +1,6 @@
 import requests
 
 def test_external_ping():
-    resp = requests.get("https://httpbin.org/get", timeout=5)
+    resp = requests.get("https://api.github.com", timeout=5)
+
     assert resp.status_code == 200
